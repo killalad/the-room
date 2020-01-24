e1c7cabbca8abfd16134d5269308a33289c1be73
Martin Georgiu <login@georgiu.cz>
2020-01-24 13:48:13 +0100

    new library #1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gpio = require('rpi-gpio')
+const rpio = require('rpio')
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
@@ -10,10 +11,12 @@ const remoteServer = require('socket.io-client')(
 	'https://' + process.env.SERVER_URL + '/',
 )
 
-gpio.setup(10, gpio.DIR_OUT)
-gpio.setup(8, gpio.DIR_OUT)
-gpio.setup(36, gpio.DIR_IN, gpio.EDGE_RISING)
-gpio.setup(38, gpio.DIR_IN, gpio.EDGE_BOTH)
+// gpio.setup(10, gpio.DIR_OUT)
+rpio.open(10, rpio.OUTPUT, rpio.LOW)
+// gpio.setup(8, gpio.DIR_OUT)
+rpio.open(8, rpio.OUTPUT, rpio.LOW)
+// gpio.setup(36, gpio.DIR_IN, gpio.EDGE_RISING)
+// gpio.setup(38, gpio.DIR_IN, gpio.EDGE_BOTH)
 
 server.listen(process.env.LOCAL_PORT)
 
@@ -29,22 +32,26 @@ let values = {
 	mainLight: false,
 	tableLight: false,
 }
+let convert = {
+	true: rpio.HIGH,
+	false: rpio.LOW,
+}
 let lastTime = 0
 
-remoteServer.on('toggle', function(data) {
-	data = JSON.parse(crypto.decrypt(data))
-	if (Date.now() - lastTime > 200) {
-		lastTime = Date.now()
-		values[data.type] = data.value
-		io.sockets.emit('send-data', values)
-		gpio.write(pins[data.type], data.value, function(err) {
-			if (err) console.log(err)
-		})
-	}
-})
-remoteServer.on('request-data', function() {
-	remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
-})
+// remoteServer.on('toggle', function(data) {
+// 	data = JSON.parse(crypto.decrypt(data))
+// 	if (Date.now() - lastTime > 200) {
+// 		lastTime = Date.now()
+// 		values[data.type] = data.value
+// 		io.sockets.emit('send-data', values)
+// 		gpio.write(pins[data.type], data.value, function(err) {
+// 			if (err) console.log(err)
+// 		})
+// 	}
+// })
+// remoteServer.on('request-data', function() {
+// 	remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+// })
 
 io.on('connection', function(socket) {
 	socket.emit('send-data', values)
@@ -55,35 +62,36 @@ io.on('connection', function(socket) {
 			values[data.type] = data.value
 			socket.broadcast.emit('send-data', values)
 			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
-			gpio.write(pins[data.type], data.value, function(err) {
-				if (err) console.log(err)
-			})
+			// gpio.write(pins[data.type], data.value, function(err) {
+			// 	if (err) console.log(err)
+			// })
+			rpio.write(pins[data.type], convert[data.value])
 		}
 	})
-	gpio.on('change', function(channel, value) {
-		if (Date.now() - lastTime > 200) {
-			lastTime = Date.now()
-			if (channel === 36) {
-				Object.keys(values).forEach(key => {
-					if (values[key] === true) {
-						values[key] = false
-						gpio.write(pins[key], false, function(err) {
-							if (err) console.log(err)
-						})
-					}
-				})
-				socket.broadcast.emit('send-data', values)
-				remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
-			}
-			if (channel === 38) {
-				values['mainLight'] = !values['mainLight']
+	// gpio.on('change', function(channel, value) {
+	// 	if (Date.now() - lastTime > 200) {
+	// 		lastTime = Date.now()
+	// 		if (channel === 36) {
+	// 			Object.keys(values).forEach(key => {
+	// 				if (values[key] === true) {
+	// 					values[key] = false
+	// 					gpio.write(pins[key], false, function(err) {
+	// 						if (err) console.log(err)
+	// 					})
+	// 				}
+	// 			})
+	// 			socket.broadcast.emit('send-data', values)
+	// 			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+	// 		}
+	// 		if (channel === 38) {
+	// 			values['mainLight'] = !values['mainLight']
 
-				gpio.write(pins['mainLight'], values['mainLight'], function(err) {
-					if (err) console.log(err)
-				})
-				socket.broadcast.emit('send-data', values)
-				remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
-			}
-		}
-	})
+	// 			gpio.write(pins['mainLight'], values['mainLight'], function(err) {
+	// 				if (err) console.log(err)
+	// 			})
+	// 			socket.broadcast.emit('send-data', values)
+	// 			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+	// 		}
+	// 	}
+	// })
 })
