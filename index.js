17080942674e2b40dcc678be6a95852d120f1b69
Martin Georgiu <login@georgiu.cz>
2020-01-25 11:09:38 +0100

    best auth #1

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const crypto = require('./encryption')
-
+const { logger } = require('./logger.js')
 require('dotenv').config()
 const remoteServer = require('socket.io-client')(
 	'https://' + process.env.SERVER_URL + '/?token=' + process.env.AUTH_TOKEN,
 )
 
+// GPIO SETUP
 rpio.open(10, rpio.OUTPUT, rpio.LOW)
 rpio.open(8, rpio.OUTPUT, rpio.LOW)
 rpio.open(36, rpio.INPUT, rpio.PULL_UP)
 rpio.open(38, rpio.INPUT, rpio.PULL_UP)
+rpio.poll(36, haltAll, rpio.POLL_HIGH)
+rpio.poll(38, mainSwitch)
 
-rpio.poll(36, switchers, rpio.POLL_HIGH)
-rpio.poll(38, switchers)
-
+// EXPRESS
 server.listen(process.env.LOCAL_PORT)
-
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
-
+// VALUES AND CONSTANTS
 const pins = {
 	mainLight: 10,
 	tableLight: 8,
@@ -38,12 +37,12 @@ let convert = {
 }
 let lastTime = 0
 
-function switchers(pin) {
+//	BUTTON FUNCTIONS
+function haltAll() {
 	rpio.msleep(20)
-
 	if (Date.now() - lastTime > 200) {
 		lastTime = Date.now()
-		if (pin === 36) {
+		try {
 			Object.keys(values).forEach(key => {
 				if (values[key] === true) {
 					values[key] = false
@@ -52,44 +51,63 @@ function switchers(pin) {
 			})
 			io.sockets.emit('send-data', values)
 			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+		} catch (e) {
+			logger.warn(e)
 		}
-		if (pin === 38) {
+	}
+}
+function mainSwitch() {
+	rpio.msleep(20)
+	if (Date.now() - lastTime > 200) {
+		lastTime = Date.now()
+		try {
 			values['mainLight'] = !values['mainLight']
 			rpio.write(pins['mainLight'], convert[values['mainLight']])
 			io.sockets.emit('send-data', values)
 			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+		} catch (e) {
+			logger.warn(e)
 		}
 	}
 }
 
+// REMOTE SOCKET.IO
 remoteServer.on('toggle', function(data) {
-	try {
-		data = JSON.parse(crypto.decrypt(data))
-	} catch (e) {
-		console.log(e)
-	}
-
 	if (Date.now() - lastTime > 200) {
 		lastTime = Date.now()
-		values[data.type] = data.value
-		io.sockets.emit('send-data', values)
-		rpio.write(pins[data.type], convert[data.value])
+		try {
+			data = JSON.parse(crypto.decrypt(data))
+			values[data.type] = data.value
+			io.sockets.emit('send-data', values)
+			rpio.write(pins[data.type], convert[data.value])
+		} catch (e) {
+			logger.warn(e)
+		}
 	}
 })
 remoteServer.on('request-data', function() {
-	remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+	try {
+		remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+	} catch (e) {
+		logger.warn(e)
+	}
 })
 
+// LOCAL SOCKET.IO
 io.on('connection', function(socket) {
 	socket.emit('send-data', values)
 
 	socket.on('toggle', function(data) {
 		if (Date.now() - lastTime > 200) {
 			lastTime = Date.now()
-			values[data.type] = data.value
-			socket.broadcast.emit('send-data', values)
-			remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
-			rpio.write(pins[data.type], convert[data.value])
+			try {
+				values[data.type] = data.value
+				socket.broadcast.emit('send-data', values)
+				remoteServer.emit('send-data', crypto.encrypt(JSON.stringify(values)))
+				rpio.write(pins[data.type], convert[data.value])
+			} catch (e) {
+				logger.warn(e)
+			}
 		}
 	})
 })
